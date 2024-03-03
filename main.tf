3116edf049a7c88f7c48faf268c05da9312e5391
ppdx999 <ppdx999@gmail.com>
2024-03-03 13:03:10 +0900

    single web server

--- main.tf
+++ main.tf
@@ -5,8 +5,39 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.example.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
     Name = "example-instance"
   }
+}
+
+resource "aws_security_group" "example" {
+  name = "example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use"
+  type        = number
+  default     = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP of the web server"
 }
